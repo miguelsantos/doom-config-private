fbdd8292c6b6014530a99a721efbe7d31b46e2d7
Miguel Santos <miguel@miguelsantos.com.br>
2021-05-24 15:31:18 -0300

    ue4: renaming

--- modules/tools/ue4/config.el
+++ modules/tools/ue4/config.el
@@ -1,7 +1,9 @@
 ;;; tools/ue4/config.el -*- lexical-binding: t; -*-
 
-(defun +ue4-uproject-p (dir)
-  "Is directory part of UE4 project?"
+(defun +ue4-expand-uproject-name (dir)
+  "Expand *.uproject file name in DIR.
+
+Returns it's absolute, canonnicalized form."
   (file-expand-wildcards (expand-file-name "*.uproject" dir)))
 
 (def-project-mode! +ue4-mode
@@ -10,6 +12,4 @@
            c++-mode
            csharp-mode
            json-mode)
-  :when (locate-dominating-file default-directory #'+ue4-uproject-p))
-
-(locate-dominating-file "C:\\Users\\msan\\projects\\Dirk\\Source\\DirkCore\\Private\\Log.h" #'+ue4-uproject-p)
+  :when (locate-dominating-file default-directory #'+ue4-expand-uproject-name))
